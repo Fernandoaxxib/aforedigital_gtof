8e87d71d185e8cb172ba7bdb84fc60e056fdc60a
JesusBanorte <74919041+JesusBanorte@users.noreply.github.com>
2021-02-11 11:28:56 -0600

    sesion timeout redirect login

--- src/main/java/mx/axxib/aforedigitalgt/ctrll/SecurityConfig.java
+++ src/main/java/mx/axxib/aforedigitalgt/ctrll/SecurityConfig.java
@@ -7,6 +7,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.session.InvalidSessionStrategy;
+
+import mx.axxib.aforedigitalgt.com.JsfRedirectStrategy;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,17 +17,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		     http.authorizeRequests().antMatchers("/javax.faces.resource/**", "/login*", "api/*","resources/**","css/**").permitAll()
-		     .anyRequest()
-				.authenticated().and()
-				.formLogin()
-				.loginPage("/login.jsf")
-				.defaultSuccessUrl("/home")
-				.failureUrl("/login.jsf?error=true")
+		     http
+		     	.authorizeRequests()
+			     	.antMatchers("/javax.faces.resource/**", "/login*", "api/*","resources/**","css/**").permitAll()
+			     	.anyRequest().authenticated()
+		     	.and().exceptionHandling().accessDeniedPage("/error")
+		     	.and().formLogin()
+		     		.loginPage("/login.jsf")
+		     		.defaultSuccessUrl("/home")
+		     		.failureUrl("/login.jsf?error=true")
 				.and().logout()
-				.logoutSuccessUrl("/login.jsf").and()
-				.csrf().disable();
-
+					.logoutSuccessUrl("/login.jsf")
+					.invalidateHttpSession(true)
+				.and().csrf().disable();
+		     
+		     InvalidSessionStrategy invalidSessionStrategy = new JsfRedirectStrategy();
+		     
+			http.sessionManagement().invalidSessionStrategy(invalidSessionStrategy )
+		        .invalidSessionUrl("/invalidSession.html")
+		     	.maximumSessions(1).expiredUrl("/login?expired=true");
 	}
 
 	@Autowired
