3452f7818358dea8d51c8779037b2e160fe77dd7
JesusBanorte <74919041+JesusBanorte@users.noreply.github.com>
2021-09-20 08:55:18 -0500

    exception

--- src/main/java/mx/axxib/aforedigitalgt/com/AforeException.java
+++ src/main/java/mx/axxib/aforedigitalgt/com/AforeException.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class AforeException extends Exception {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L; 
 
 	@Getter
 	@Setter
